a70047d0792d54102a637037fa3cbf4a5c71af07
Alex Chen <achen2291@gmail.com>
2026-06-30 22:13:31 -0700

    ShowSelection

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,18 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({
+  shows,
+  selectedShow,
+  setSelectedShow,
+}) {
+  return (
+    <nav className="shows">
+      {shows.map((show) => (
+        <section key={show.name} onClick={() => setSelectedShow(show)}>
+          {show.name}
+        </section>
+      ))}
+    </nav>
+  );
 }
